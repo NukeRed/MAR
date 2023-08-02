bb3113283846886207a225a1d3d31a3b732b9f85
AlexanderDerkun <38619024+AlexanderDerkun@users.noreply.github.com>
2023-08-02 13:19:22 +0300

    edit MainActivity.kt and add navController for Help

--- app/src/main/java/com/derkun/mar/MainActivity.kt
+++ app/src/main/java/com/derkun/mar/MainActivity.kt
@@ -3,12 +3,27 @@ package com.derkun.mar
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.android.material.appbar.MaterialToolbar
-
+import android.view.Menu
+import android.view.MenuItem
+import androidx.navigation.findNavController
+import androidx.navigation.ui.onNavDestinationSelected
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_toolbar,menu)
+        return super.onCreateOptionsMenu(menu)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return super.onOptionsItemSelected(item)
+                || super.onOptionsItemSelected(item)
     }
 }
